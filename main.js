362c6549f6663049f3d5388d536b4eb201cf4f70
Aiden Melendez <itm5305@rit.edu>
2018-04-05 03:05:49 -0400

    initial colors and random living and dead cells work

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const THREE = require( 'three' )
 
 const app = {
   init() {
-    this.gridSize = 5
+    this.debug = true
+    this.once = false
+    this.gridSize = 20
     
     this.geometries = []
     this.meshes = []
@@ -14,12 +16,12 @@ const app = {
     
     // Asssign random values to each cell initially 
     for (let i = 0; i < this.gridSize; i++) {
-        this.currentGrid[i] = [];
-        this.nextGrid[i] = [];
+        this.currentGrid[i] = []
+        this.nextGrid[i] = []
 
         for (let j = 0; j < this.gridSize; j++) {
-            this.currentGrid [i][j] = Math.random() > .5 ? 1 : 0;
-            this.nextGrid[i][j] = 0;
+            this.currentGrid [i][j] = Math.random() > .5 ? 1 : 0
+            this.nextGrid[i][j] = 0
         }
     }
     
@@ -40,16 +42,19 @@ const app = {
     this.createLights()
     
     const material = new THREE.MeshPhongMaterial({ color:0x009999 })
-    const materialDead = new THREE.MeshPhongMaterial({ color:0x000000 })
+    const materialDead = new THREE.MeshPhongMaterial({ color:0x404040 })
     
     // Make grid of geometry
     for (let i = 0; i < this.gridSize; i++) {
       this.geometries[i] = []
       this.meshes[i] = []
       
-      for (let j = 0; j < this.gridSize; j++){
+      for ( let j = 0; j < this.gridSize; j++ ){
         this.geometries[i][j] = new THREE.IcosahedronGeometry( .5, 0 )
-        this.meshes[i][j] = new THREE.Mesh( this.geometries[i][j], material )
+        if (this.currentGrid[i][j] == 0)
+          this.meshes[i][j] = new THREE.Mesh( this.geometries[i][j], materialDead )
+        if (this.currentGrid[i][j] == 1)
+          this.meshes[i][j] = new THREE.Mesh( this.geometries[i][j], material )
       }
     }
     
@@ -64,12 +69,14 @@ const app = {
     this.render()
   },
   
+ 
+  
   createRenderer() {
     this.renderer = new THREE.WebGLRenderer()
     this.renderer.setSize( window.innerWidth, window.innerHeight )
 	  document.body.appendChild( this.renderer.domElement )
     this.render = this.render.bind( this )
-	},
+  },
   
   createLights() {
     this.pointLight = new THREE.PointLight( 0xffffff, .5 )
@@ -81,8 +88,10 @@ const app = {
     
   },
   
+  
   render() {
     window.requestAnimationFrame( this.render )
+    //this.animate();
     
     // change positions
     for (let i = 0; i < this.gridSize; i++) {
@@ -90,6 +99,12 @@ const app = {
         this.meshes[i][j].position.y = j
         this.meshes[i][j].rotation.x += .005
         this.meshes[i][j].rotation.y += .005
+        
+        // change to correct color
+        if ( this.currentGrid[i][j] == 0 )  // if alive
+          this.meshes[i][j].material.color.setHex( 0x999900 )
+        else if ( this.currentGrid[i][j] == 1 ) // if dead
+           this.meshes[i][j].material.color.setHex( 0x404040 )
         
         if (i == 0) {
           this.meshes[i][j].position.x = 0
